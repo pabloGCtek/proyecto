0853829d82da513d154f06f29bd93fa25cb821a2
raulgomezcano <raul.gomezc@softtek.com>
2023-05-20 16:12:21 +0200

    Servicio añadido pero comentado, no funciona

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
+import { GalleryComponent } from './gallery/gallery.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    ContactComponent,
+    GalleryComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
